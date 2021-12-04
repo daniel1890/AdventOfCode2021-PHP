469a4524f38049b667db97027ad947c7482dc48a
Daniël <59751559+daniel1890@users.noreply.github.com>
2021-12-04 15:04:16 +0100

    Add files via upload

---
+++ dag3.php
@@ -0,0 +1,13 @@
+<?php
+
+// Pas dag.txt aan naar bijbehorende dag.
+$lijst = explode("\n", file_get_contents('dag3.txt'));
+
+$ans1 = 0;
+$ans2 = 0;
+
+echo "Antwoord op vraag 1: $ans1";
+echo "</br>";
+echo "Antwoord op vraag 2: $ans2";
+
+?>
