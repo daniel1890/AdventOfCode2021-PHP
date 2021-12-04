9a5305a19ea191bedd97205a14c44c4e1c1f5b10
Daniël <59751559+daniel1890@users.noreply.github.com>
2021-12-04 15:52:58 +0100

    Finish dag3 deel 1.

--- dag3.php
+++ dag3.php
@@ -6,6 +6,46 @@ $lijst = explode("\n", file_get_contents('dag3.txt'));
 $ans1 = 0;
 $ans2 = 0;
 
+function bepaalEpsilonEnGammaEnVermenigVuldigDeze($lijst, $ans) {
+    $aantalChars = strlen($lijst[0]);
+    $epsilonArray = array();
+    $gammaArray = array();
+
+    $epsilonString = '';
+    $gammaString = '';
+
+    for($i = 0; $i < $aantalChars - 1 ; $i++) {
+        $nul = 0;
+        $een = 0;
+
+        foreach($lijst as $index => $regel) {
+            $splitWoord = str_split($regel);
+
+            $splitWoord[$i] == 0 ? ($nul += 1) : ($een += 1);
+        }
+        $nul > $een ? ($epsilonArray[] = 0) & ($gammaArray[] = 1) : ($epsilonArray[] = 1) & ($gammaArray[] = 0);
+
+        $epsilonString .= $epsilonArray[$i];
+        $gammaString .= $gammaArray[$i];
+
+        echo $epsilonString;
+        $nul = 0;
+        $een = 0;
+    }
+
+    $gamma = $gammaString;
+    $epsilon = $epsilonString;
+
+    $gammaDecimaal = bindec($gamma);
+    $epsilonDecimaal = bindec($epsilon);
+
+    $ans = $gammaDecimaal * $epsilonDecimaal;
+
+    return $ans;
+}
+
+$ans1 = bepaalEpsilonEnGammaEnVermenigVuldigDeze($lijst, $ans1);
+
 echo "Antwoord op vraag 1: $ans1";
 echo "</br>";
 echo "Antwoord op vraag 2: $ans2";
